1b13f91978c8cb8e6a983ceb51028ed6fbb6f591
Alethia Quintero <alethiaq22@gmail.com>
2019-07-03 01:37:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,11 @@ end
 
 def summon_captain_planet(array)
   array.map {|planet| planet.capitalize + "!"}
-  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.map {|c| 
+  if c.size > 4 
 end
 
 def find_the_cheese# code an argument here
